537682e7f8f2fa33e4cc4ea5c74331d6f1a8913c
CP Knight <chris@netcog.net>
2016-11-16 10:14:45 -0700

    Initial substantive commit
    First substantive commit of code. Untested.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ForeFlight-Logbook-Parser
 Script to parse ForeFlight logbook data. Currently exports to SQL.
+
+*parseForeFlightLog.php* - parses ForeFlight logbook data and spits out 
+      MySQL queries. This supports my logbook-output project. Subject to 
+      the BSD 3-clause license in LICENSE.
+
+```
+    Usage: parseForeFlightLog.php [ForeFlight Logbook Import CSV File]
+       eg. parseForeFlightLog.php logbook_na,e.csv
+```
+
+Why PHP, you ask? Because it's a convenient language for this particular
+exercise. Just another tool in the toolbox. Nothing more, nothing less.
+cf. `https://slack.engineering/taking-php-seriously-cf7a60065329#.2jer4rc28`
---
+++ parseForeFlightLog.php
@@ -0,0 +1,119 @@
+#!/usr/bin/php
+<?php // ----------------------------------------------------------------------
+//   parseForeFlightLog.php - parses ForeFlight logbook data and spits out 
+//      MySQL queries. This supports my logbook-output project. Subject to 
+//      the BSD 3-clause license in LICENSE. 
+// ---------------------------------------------------------------------------
+
+ini_set('memory_limit','512M');
+ini_set("auto_detect_line_endings", true);
+date_default_timezone_set ("UTC");
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if (!isset($argv[1]))
+{
+  echo "Usage: " . $argv[0] . " [ForeFlight Logbook Import CSV File]\n";
+  echo "   eg. " . $argv[0] . " logbook_default.csv\n"; 
+  exit(-1);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+$lines = file($argv[1]);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// First, determine where to look for aircraft and flights data.
+
+$aircraftDataLine = -1;
+$flightDataLine = -1;
+
+foreach ($lines as $lineNumber => $line) 
+{
+  if (substr($line, 0, 14) === "Aircraft Table")
+    $aircraftDataLine = $lineNumber;
+
+  if (substr($line, 0, 13) === "Flights Table")
+    $flightDataLine = $lineNumber;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Second, we parse the CSV data and put stuff into arrays.
+
+$aircraftCSVData = array();
+$flightCSVData = array();
+
+foreach ($lines as $lineNumber => $line) 
+{
+  if (($lineNumber > $aircraftDataLine) and ($lineNumber < ($flightDataLine - 1)))
+    array_push($aircraftCSVData,$line);
+  else if ($lineNumber > $flightDataLine)
+    array_push($flightCSVData,$line);
+}
+
+$aircraftData = csvToArray($aircraftCSVData);
+$flightData = csvToArray($flightCSVData);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Next, we build a big long SQL string, and dump that to STDOUT.
+
+$sql = arrayToMySQLCreateTable("Aircraft", $aircraftData);
+$sql .= arrayToMySQLInsertInto("Aircraft", $aircraftData);
+$sql .= arrayToMySQLCreateTable("Flights", $flightData);
+$sql .= arrayToMySQLInsertInto("Flights", $flightData);
+print $sql;
+
+// ---------------------------------------------------------------------------
+// Function to convert the CSV data into an array. 
+// ---------------------------------------------------------------------------
+
+function csvToArray($data)
+{
+  $csv = array_map('str_getcsv', $data);
+  array_walk($csv, function(&$a) use ($csv) {
+    $a = array_combine($csv[0], $a);
+  });
+  array_shift($csv); # remove column header
+  return $csv;
+}
+
+// ---------------------------------------------------------------------------
+// Function to convert the array into MySQL INSERT INTO queries.
+// ---------------------------------------------------------------------------
+
+function arrayToMySQLInsertInto($tableName, $data)
+{
+  foreach ($data as $entry)
+  {
+    $columns = implode(", ",array_keys($entry));
+    $escaped_values = array_map('mysql_escape_string', array_values($entry)); 
+    // above line is not designed to be run with a db connection /:. not as safe as: 
+    // $escaped_values = array_map('mysql_real_escape_string', array_values($entry));
+    $values  = implode(", ", $escaped_values);
+    $sql .= "INSERT INTO `$tableName` ($columns) VALUES ($values);\n";  
+  }
+
+  return $sql;
+}
+
+// ---------------------------------------------------------------------------
+// Function to build a CREATE TABLE MySQL query;
+// ---------------------------------------------------------------------------
+
+function arrayToMySQLCreateTable($tableName, $data)
+{
+  $columns = array_keys($data[1]);
+
+  $sql = "CREATE TABLE `$tableName` ( ";
+  foreach ($columns as $column)
+    if ($column != "")
+      $sql .= "`$column` TEXT,"; // note all are text for this purpose. YMMV.
+
+  $sql = substr($sql, 0, strlen($sql) -1); // remove extra ,
+  $sql .= ");\n";
+
+  return $sql;
+}
+
+// ---------------------------------------------------------------------------
+//                                                                         eof ?>
